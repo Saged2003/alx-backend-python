66284679ef7cec950a5108ead57f4fa8b6df5d32
Saged2003 <sagedryan775@gmail.com>
2024-08-26 16:49:48 -0700

    saged

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,13 @@
-
 #!/usr/bin/env python3
-'''Asyncronous Python
-'''
-from typing import List
+"""wait for some tasks"""
+
 import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-time_wait_random = __import__('3-tasks').task_wait_random
-
-
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''return a list of awaited response from previous function
-    '''
-    res = await asyncio.gather(
-        *tuple(map(lambda _: time_wait_random(max_delay), range(n)))
-    )
-    return sorted(res)
-
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Waits for random delays"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
